af39c2cdf078fb349bef9cb9937e1e3b29d7999d
musharraf504 <124203520+musharraf504@users.noreply.github.com>
2023-05-24 09:36:10 +0530

    modify bootsrap

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 let posts = [];
 
 const app = express();
+app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 
 app.use(express.urlencoded({extended: true}));
-app.use(express.static("public"));
 
 
 
